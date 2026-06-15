9dfc313d832246532c420f65d5861ccbe77ff167
Anvar <soxibovanvar1009@gmail.com>
2026-06-15 23:35:19 +0500

    Exception Middleware finished

--- Application/Services/AuthService.cs
+++ Application/Services/AuthService.cs
@@ -1,4 +1,5 @@
 ﻿using Application.DTOs.RegisterDTO;
+using Application.Exceptions;
 using Application.Interfaces.Repositories_interface;
 using Application.Interfaces.Security;
 using Application.Interfaces.ServiceInterface;
@@ -19,9 +20,22 @@ namespace Application.Services
             passwordHasher = hasher;
             
         }
-
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="requestRegisterDTO"></param>
+        /// <returns></returns>
+        /// <exception cref="LoginAlreadyExistsException"></exception>
         public async Task<ResponseRegisterDTO> Register_Service(RequestRegisterDTO requestRegisterDTO)
         {
+            // bu login mavjudligini tekshiradi
+            var existingUser = await _userRepositories.GetByLoginAsycn(requestRegisterDTO.Login);
+
+            if (existingUser != null)
+            {
+                throw new LoginAlreadyExistsException();
+            }
+
             User user = new User()
             {
                 FirstName = requestRegisterDTO.FirstName,
@@ -48,15 +62,31 @@ namespace Application.Services
             return responseRegisterDTO;
         }
 
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="loginRequest"></param>
+        /// <returns></returns>
+        /// <exception cref="InvalidCredentialsException"></exception>
         public async Task<LoginResponseDTO> Login_Service(LoginRequestDTO loginRequest)
         {
             User user = await _userRepositories.GetByLoginAsycn(loginRequest.Login);
 
+            if (user == null)
+            {
+                throw new InvalidCredentialsException();
+            }
+
             bool verify = passwordHasher.VerifyPassword(loginRequest.Password, user.PasswordHash);
 
-            if (user is not null && verify)
+            if (!verify)
             {
+                throw new InvalidCredentialsException();
+            }
 
+           
                 LoginResponseDTO responseDTO = new LoginResponseDTO()
                 {
                     UserId = user.Id,
@@ -65,9 +95,7 @@ namespace Application.Services
                 };
 
                 return responseDTO;
-            }
-
-            return null;
+           
         }
     }
 }
