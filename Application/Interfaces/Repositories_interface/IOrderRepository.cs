fd2fef24c3fa603ea23b62ac9c6a15f79aade7ef
Anvar <soxibovanvar1009@gmail.com>
2026-06-30 22:25:54 +0500

    add order repository

---
+++ Application/Interfaces/Repositories_interface/IOrderRepository.cs
@@ -0,0 +1,20 @@
+﻿using Domain.Models.OrdersModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Interfaces.Repositories_interface
+{
+    public interface IOrderRepository
+    {
+        Task CreateAsync(Order order);
+
+        Task<List<Order>> GetBuyerOrders(int buyerId);
+
+        Task<List<Order>> GetSellerOrders(int sellerId);
+
+        Task<Order?> GetByIdAsync(int id);
+    }
+}
---
+++ Infrastructure/Repositories/OrderRepoFolder/OrderRepository.cs
@@ -0,0 +1,33 @@
+﻿using Application.Interfaces.Repositories_interface;
+using Domain.Models.OrdersModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Repositories.OrderRepoFolder
+{
+    public class OrderRepository : IOrderRepository
+    {
+        public Task CreateAsync(Order order)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Order>> GetBuyerOrders(int buyerId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Order?> GetByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Order>> GetSellerOrders(int sellerId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
