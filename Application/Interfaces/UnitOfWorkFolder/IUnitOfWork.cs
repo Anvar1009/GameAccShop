0654f6a000e93a83d35c26209384c5dc1c8f9716
Anvar1009 <soxibovanvar1009@gmail.com>
2026-07-01 20:57:41 +0500

    add UnitOfWork

---
+++ Application/Interfaces/UnitOfWorkFolder/IUnitOfWork.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore.Storage;
+
+namespace Application.Interfaces.UnitOfWorkFolder
+{
+    public interface IUnitOfWork
+    {
+        Task<int> SaveChangesAsync();
+
+        Task<IDbContextTransaction> BeginTransactionAsync();
+    }
+}
