eb0d1050257901012cf8975e83f4627e55f5d60e
Anvar1009 <soxibovanvar1009@gmail.com>
2026-07-02 18:27:38 +0500

    add UnitOfWork for Transactions

---
+++ Application/Interfaces/ServiceInterface/IOrderService.cs
@@ -0,0 +1,24 @@
+﻿using Application.DTOs.OrderDTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Interfaces.ServiceInterface
+{
+    public interface IOrderService
+    {
+        Task<OrderResponse> CreateOrderAsync(CreateOrderRequest request);
+
+        Task<OrderDetailsResponse> GetByIdAsync(int orderId);
+
+        Task<List<BuyerOrderResponse>> GetBuyerOrdersAsync(int buyerId);
+
+        Task<List<SellerOrderResponse>> GetSellerOrdersAsync(int sellerId);
+
+        Task CancelOrderAsync(int orderId);
+
+        Task ConfirmOrderAsync(int orderId);
+    }
+}
---
+++ Infrastructure/Services/UnitOfWork.cs
@@ -0,0 +1,31 @@
+﻿using Application.Interfaces.UnitOfWorkFolder;
+using Infrastructure.EntityModel;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Services
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly DbContextModel _dbContext;
+        public UnitOfWork(DbContextModel dbContext)
+        {
+            _dbContext = dbContext;
+            
+        }
+        public async Task<IDbContextTransaction> BeginTransactionAsync()
+        {
+            return await _dbContext.Database.BeginTransactionAsync();
+        }
+
+        public async Task<int> SaveChangesAsync()
+        {
+            return await _dbContext.SaveChangesAsync();
+        }
+    }
+}
