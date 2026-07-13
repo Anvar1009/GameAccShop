c09e72902c1ff7070ee75bf6406b0a0113cd2abd
Anvar1009 <soxibovanvar1009@gmail.com>
2026-07-13 11:30:40 +0500

    add MessageRepository

---
+++ Infrastructure/Repositories/MessageRepository.cs
@@ -0,0 +1,52 @@
+﻿using Application.Interfaces.Repositories_interface;
+using Domain.Models.OrdersModel;
+using Infrastructure.EntityModel;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Repositories
+{
+    public class MessageRepository : IMessageRepository
+    {
+        private readonly DbContextModel _dbContext;
+
+        public MessageRepository(DbContextModel dbContextModel)
+        {
+            _dbContext = dbContextModel;            
+        }
+        public async Task<Message> CreateAsync(Message message)
+        {
+            await _dbContext.Messages.AddAsync(message);
+            await _dbContext.SaveChangesAsync();
+            return message;
+        }
+
+        public async Task<List<Message>> GetMessagesAsync(int conversationId)
+        {
+            return await _dbContext.Messages
+                .Where(m => m.ConversationId == conversationId)
+                .OrderBy(m => m.CreatedAt)
+                .ToListAsync();
+        }
+
+        public async Task<int> GetUnreadCountAsync(int conversationId, int userId)
+        {
+            var count = await _dbContext.Messages
+                .Where(m => m.ConversationId == conversationId && m.SenderId != userId && !m.IsRead)
+                .CountAsync();
+            return count;
+        }
+
+        public async Task MarkAsReadAsync(int conversationId, int userId)
+        {
+            await _dbContext.Messages
+                .Where(m => m.ConversationId == conversationId && m.SenderId != userId && !m.IsRead)
+                .ExecuteUpdateAsync(x =>x.SetProperty(m => m.IsRead, true));
+            await _dbContext.SaveChangesAsync();
+        }
+    }
+}
