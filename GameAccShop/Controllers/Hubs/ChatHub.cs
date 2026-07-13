77e26369bf539ecd1637d32625f7e5ec59cedfdc
Anvar1009 <soxibovanvar1009@gmail.com>
2026-07-13 17:49:03 +0500

    add model ChatHub

---
+++ GameAccShop/Controllers/Hubs/ChatHub.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using Microsoft.EntityFrameworkCore;
+using System.Text.RegularExpressions;
+
+namespace GameAccShop.Controllers.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public async Task JoinConversation(int conversationId)
+        {
+            await Groups.AddToGroupAsync(
+                Context.ConnectionId,
+                $"conversation-{conversationId}");
+        }
+
+        public async Task LeaveConversation(int conversationId)
+        {
+            await Groups.RemoveFromGroupAsync(
+                Context.ConnectionId,
+                $"conversation-{conversationId}");
+        }
+    }
+}
