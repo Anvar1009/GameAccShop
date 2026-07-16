87cbaffd116ec662f09ca36fbfaa524cb45cdc68
Anvar1009 <soxibovanvar1009@gmail.com>
2026-07-16 13:01:02 +0500

    add model Notification

---
+++ Domain/Models/ChatModels/Notification.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Models.ChatModels
+{
+    public class Notification
+    {
+        public int Id { get; set; } 
+
+        public int userId { get; set; }
+        public string Title { get; set; }
+        public string Message { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public bool IsRead { get; set; }
+
+    }
+}
